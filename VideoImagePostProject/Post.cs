392099e056106a3256ce36315c81befd8ab1a942
Timi Adiel <timiadielluv.nd@gmail.com>
2021-04-15 00:47:20 +0100

    Add files via upload

---
+++ VideoImagePostProject/Post.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VideoImagePostProject
+{
+    class Post
+    {
+        private static int currentPostId;
+
+        //properties
+        protected int ID { get; set; }
+        protected string Title { get; set; }
+        protected string SendByUsername { get; set; }
+        public bool IsPublic { get; set; }
+
+
+        //Default constructor, if a derived class does not invoke a base-
+        //class constructor explicitly, the default constructor is called
+        //implicitly.
+
+        public Post()
+        {
+            ID = 0;
+            Title = "My First Post";
+            IsPublic = true;
+            SendByUsername = "Timi Adiel";
+
+        }
+
+        //Instance constructor that has three parameter
+        public Post(string title, bool isPublic, string sendByUsername)
+        {
+            this.ID = GetNextID();
+            this.Title = title;
+            this.SendByUsername = sendByUsername;
+            this.IsPublic = isPublic;
+        }
+
+        //ID should not be zero instead it should be unique create a method
+        protected int GetNextID()
+        {
+            return ++currentPostId;
+        }
+
+        //to edit the text in our post we create a method
+        public void Update(string title, bool isPublic)
+        {
+            this.Title = title;
+            this.IsPublic = isPublic;
+        }
+
+        //vertual method override of 2 string inherited from system.object
+        public override string ToString()
+        {
+            return String.Format("{0} - {1} - by {2}", this.ID, this.Title, this.SendByUsername);
+        }
+    }
+}
---
+++ VideoImagePostProject/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace VideoImagePostProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Post post1 = new Post("Thanks for the birthday wishes", true, "Timi Adiel");
+            Console.WriteLine(post1.ToString());
+
+            ImagePost imagePost1 = new ImagePost("Check out my new shoes",
+                "Timi Adiel", "https://images.com/shoes", true);
+
+            Console.WriteLine(imagePost1.ToString());
+
+            VideoPost videoPost1 = new VideoPost("I love this video", "Timi Adiel", 
+                "https://www.youtube.com/watch?v=iSgUMPHQEWw", 10, true);
+
+            Console.WriteLine(videoPost1.ToString());
+
+            videoPost1.Play();
+            Console.WriteLine("Press any key to stop the video1");
+            Console.ReadKey();
+            videoPost1.Stop();
+
+
+            Console.ReadLine();
+        }
+    }
+}
